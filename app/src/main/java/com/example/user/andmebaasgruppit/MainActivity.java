d5dcbed793674df12b916ca90b807954dd1961af
kriss1240 <33447867+kriss1240@users.noreply.github.com>
2018-06-15 17:49:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/andmebaasgruppit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.andmebaasgruppit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hall.grupptoo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
